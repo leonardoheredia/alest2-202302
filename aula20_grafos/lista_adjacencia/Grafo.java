12b365a438df62d5b53868617f075527872d8bb3
leonardoheredia <leonardo@heredia.com.br>
2023-10-18 20:23:43 -0300

    Aula 21 - Percorrendo em Profundidade

--- aula20_grafos/lista_adjacencia/Grafo.java
+++ aula20_grafos/lista_adjacencia/Grafo.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 public class Grafo {
     private int vertices;
     private ArrayList<Integer>[] listaAdjacencia;
+    private int arestas;
     public Grafo(int vertices) {
         this.vertices = vertices;
+        this.arestas = 0;
         listaAdjacencia = new ArrayList[this.vertices];
         for (int i = 0; i < this.vertices; i++) {
             listaAdjacencia[i] = new ArrayList<>();
@@ -15,25 +17,31 @@ public class Grafo {
     public void adicionarAresta(int v, int w) {
         listaAdjacencia[v].add(w);
         listaAdjacencia[w].add(v);
+        arestas++;
     }
     public void removerAresta(int v, int w) {
-
+        if(arestas==0) return;
+        listaAdjacencia[v].remove((Object)w);
+        listaAdjacencia[w].remove((Object)v);
+        arestas--;
     }
     public boolean existeAresta(int v, int w) {
-        return false;
+        return listaAdjacencia[v].contains(w);
     }
     public int getVertices() {
         return this.vertices;
     }
     public int getArestas() {
-        return 0; //retorna o numero de arestas
+        return this.arestas;
     }
     public int grau(int vertice) {
-        return -1;
+        return listaAdjacencia[vertice].size();
     }
     public ArrayList<Integer> adjacentes(int v) {
         //retornar uma array list com os vertices adjacentes ao vertice V
-        return null;
+        ArrayList<Integer> r;
+        r = (ArrayList<Integer>) listaAdjacencia[v].clone();
+        return r;
     }
     @Override
     public String toString() {
@@ -41,7 +49,7 @@ public class Grafo {
         for (int v = 0; v < listaAdjacencia.length; v++) {
             s = s + "\n" + v + " - ";
             for (Integer vAdj:listaAdjacencia[v]) {
-                s = s + vAdj + ",";
+                s = s + vAdj + "  ";
             }
         }
         return s;
---
+++ aula21_caminhamento/BuscaEmProfundidade.java
@@ -0,0 +1,41 @@
+package aula21_caminhamento;
+
+import java.util.ArrayList;
+
+public class BuscaEmProfundidade {
+    private boolean[] visitado;
+    private int[] antecessor;
+    public BuscaEmProfundidade(GrafoNaoDirigido g, int origem) {
+        visitado = new boolean[g.getVertices()];
+        antecessor = new int[g.getVertices()];
+        for(int i=0; i<antecessor.length; i++) antecessor[i] = -1;
+
+        buscarRecursivo(g, origem);
+    }
+    private void buscarRecursivo(GrafoNaoDirigido g, int vertice) {
+        visitado[vertice] = true;
+        ArrayList<Integer> adjacentes = g.adjacentes(vertice);
+        for (int vizinho:adjacentes) {
+            if(!visitado[vizinho]) {
+                antecessor[vizinho] = vertice;
+                buscarRecursivo(g, vizinho);
+            }
+        }
+    }
+    public String getCaminhos() {
+        String r = " v visit ant\n";
+        r = r +    " - ----- ---\n";
+        for (int i = 0; i < antecessor.length; i++) {
+            r = r + " " + i + " " + visitado[i] + " " + antecessor[i] + " \n";
+        }
+        return r;
+    }
+
+}
+
+
+
+
+
+
+
